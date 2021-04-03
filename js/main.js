f729a93301fd4cb02499e2b1bad212c3cd1e2412
Charles Wu <wucharleswu@gmail.com>
2021-04-03 13:17:38 -0400

    asdfghjkl

--- js/main.js
+++ js/main.js
@@ -1,10 +1,36 @@
-$(".selectors").change(function () {
+$(".selectors").change(updateS);
+
+function allyear() {
+    let allyearvalue = $('#allyear').val();
+
+    if(allyearvalue == "true"){
+        $('#allyear').val("false");
+        document.getElementById("allyear").innerHTML = "Show All Year Instead";
+        document.getElementById("yearRange").style.display = "inline-block";
+    } else {
+        $('#allyear').val("true");
+        document.getElementById("allyear").innerHTML = "Showimg all years, Click again to select individual year";
+        document.getElementById("yearRange").style.display = "none";
+    }
+
+    updateS();
+}
+
+function updateS() {
     let partA = $('#partA').val(),
         partB = $('#partB').val(),
-        var output = document.getElementById("selectyear");
-        var slider = document.getElementById("myRange");
-        year = slider.value;
-        // year = "All";
+        year,
+        allyearvalue = $('#allyear').val();
+
+        if(allyearvalue == "false"){
+            year = $('#yearRange').val()
+        } else {
+            year = "All";
+        };
+
+        console.log("data: " + partA + "-" + partB + "(" + year + ")");
+
+        document.getElementById("selectedyear").innerHTML = year; 
 
     if (partA !== partB) {
         updatesankey(partA, partB, year);
@@ -13,4 +39,4 @@ $(".selectors").change(function () {
         document.getElementById("warning").innerHTML = "Please make sure that you are comparing different data";
     }
 
-});
+}
