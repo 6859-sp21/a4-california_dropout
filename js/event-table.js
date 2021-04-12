21f70d16c00bb17ca6d6250c8a69bc2a7a738626
Hang Jiang <hang.jiang1@ibm.com>
2021-04-12 15:03:17 -0400

    add/event-table as list

--- js/event-table.js
+++ js/event-table.js
@@ -18,46 +18,25 @@ var widthbar = document.getElementById("event").offsetWidth,
 function eventTable(year) {
     
     if (d3.select('#event').size() > 0) {
-        d3.select('#event').selectAll('table').remove();
+        d3.select('#event').selectAll('ul').remove();
         console.log("REMOVE OLD TABLE");
     }
-    var table = d3.select('#event').append('table')
+    var head = d3.select("#event").append("p").text("Key events");
+    var body = d3.select('#event').append('ul')
                     .attr("width", widthbar)
-                    .attr("height", heightbar)
-	var thead = table.append('thead')
-	var	tbody = table.append('tbody');
-    var columns = ['events'];
-
-	// append the header row
-	thead.append('tr')
-	  .selectAll('th')
-	  .data(columns).enter()
-	  .append('th')
-	    .text(function (column) { return column; });
-
+                    .attr("height", heightbar);
 
     d3.csv("sankeydata/a4-cali-dropout-events.csv").then((data)=>{
        // create a row for each object in the data
-       data = data.filter((d)=> d.year == year)
-        var rows = tbody.selectAll('tr')
-        .data(data)
-        .enter()
-        .append('tr');
+       data = data.filter((d)=> d.year == year);
 
-        // create a cell in each row for each column
-        var cells = rows.selectAll('td')
-        .data(function (row) {
-            return columns.map(function (column) {
-            return {column: column, value: row[column]};
-            });
-        })
-        .enter()
-        .append('td')
-            .attr("font-size", "5px")
-            .text(function (d) { return d.value; });
-
-    });
-    return table;
+       // create a cell in each row for each column
+        body.selectAll("li")
+            .data(data)
+            .join("li")
+                .text(d => d.events);
+    })
+    return body;
 }
 
-eventTable("2000");
+eventTable("2001");
