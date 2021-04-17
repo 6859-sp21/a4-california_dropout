104856b560c836de5334b73c99b2152c16107283
hjian42 <hjian42@icloud.com>
2021-04-16 22:44:18 -0400

    update/events

--- js/event-table.js
+++ js/event-table.js
@@ -18,25 +18,41 @@ var widthbar = document.getElementById("event").offsetWidth,
 function eventTable(year) {
     
     if (d3.select('#event').size() > 0) {
+        d3.select('#event').selectAll('p').remove();
         d3.select('#event').selectAll('ul').remove();
         console.log("REMOVE OLD TABLE");
     }
-    var head = d3.select("#event").append("p").text("Key events");
-    var body = d3.select('#event').append('ul')
-                    .attr("width", widthbar)
-                    .attr("height", heightbar);
+
+    if (year == "All") {
+        return;
+    } 
+
 
     d3.csv("./data/a4-cali-dropout-events.csv").then((data)=>{
        // create a row for each object in the data
        data = data.filter((d)=> d.year == year);
 
-       // create a cell in each row for each column
-        body.selectAll("li")
-            .data(data)
-            .join("li")
-                .text(d => d.events);
+       console.log("print data size");
+       console.log(data.length); 
+       var head = d3.select("#event").append("p").text("Key reform events"); 
+        var body = d3.select('#event').append('ul')
+        .attr("width", widthbar)
+        .attr("height", heightbar);  
+       if (data.length >= 1) {
+        // create a cell in each row for each column
+         body.selectAll("li")
+             .data(data)
+             .join("li")
+                 .text(d => d.events)
+                 .style("font-size", "10px");
+       } else {
+           body.append("li")
+           .text("No reform in " + year)
+           .style("font-size", "10px");;
+       }
     })
-    return body;
+
+    // return body;
 }
 
-eventTable("2001");
+eventTable("All");
